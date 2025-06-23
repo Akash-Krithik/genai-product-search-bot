79b3d858aad5af65adef9b3224d83dbc6f184cdb
Akash-Krithik <krithikakash03@gmail.com>
2025-06-23 15:36:46 +0530

    updates

--- vectorstore.py
+++ vectorstore.py
@@ -1,5 +1,5 @@
-from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def get_vectorstore():
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
